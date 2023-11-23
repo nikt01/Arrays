165dbd46102d900c9214534c7c4c3a3f3aefb1c0
nikt01 <dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.com>
2023-11-23 12:33:35 +0100

    modified:   index2.html 	modified:   js exercise advanced/ex8ad.js

--- js exercise advanced/ex8ad.js
+++ js exercise advanced/ex8ad.js
@@ -2,9 +2,9 @@ function sortByAge(users2) {
     users2.sort((a, b) => a.age - b.age);
 }
 let users2 = [
-{ name: "Daniel", age: 90 },
-{ name: "Damian", age: 45 },
-{ name: "Domin", age: 65 }
+{ name: "Gragas", age: 90 },
+{ name: "Garen", age: 45 },
+{ name: "Yuumi", age: 65 }
 ];
 sortByAge(users2);
 console.log(users2);
